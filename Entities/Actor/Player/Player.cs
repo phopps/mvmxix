dd47800c95f767498c48d3351126570d33f4e196
phopps <pathopps1@gmail.com>
2023-02-20 22:44:10 -0500

    added basic player left, right, and jump controls using keyboard and gamepad inputs, fixed icons, added vscode to gitignore

--- Entities/Actor/Player/Player.cs
+++ Entities/Actor/Player/Player.cs
@@ -1,13 +1,14 @@
+using System.Collections.Generic;
 using Godot;
-using System;
 
 // TODO: player movement, user input
 
 public class Player : KinematicBody2D
 {
-    [Export] float moveSpeed = 100;
-    [Export] float gravity = 2000;
-    Vector2 velocity = Vector2.Zero;
+    [Export] public float moveSpeed = 100;
+    [Export] public float gravity = 2000;
+    public Vector2 velocity = Vector2.Zero;
+    [Export] public float jumpSpeed = 550;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
@@ -15,9 +16,44 @@ public class Player : KinematicBody2D
 
     }
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//
-//  }
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(float delta)
+    {
+
+    }
+
+    // Called 60 times per second, independent of framerate.
+    // 'delta' is the elapsed time since '_PhysicsProcess' was last called.
+    public override void _PhysicsProcess(float delta)
+    {
+        // reset horizontal velocity
+        velocity.x = 0;
+
+        // set horizontal velocity
+        if (Input.IsActionPressed("moveRight"))
+        {
+            velocity.x = velocity.x + moveSpeed;
+        }
+        if (Input.IsActionPressed("moveLeft"))
+        {
+            velocity.x = velocity.x - moveSpeed;
+        }
+
+        // apply gravity
+        velocity.y = velocity.y + gravity * delta;
+
+        // jump on next frame
+        if (Input.IsActionJustPressed("jump"))
+        {
+            // check if player is on the floor
+            if (IsOnFloor())
+            {
+                // negative y values are up
+                velocity.y = -jumpSpeed;
+            }
+        }
+
+        // move player ('MoveAndSlide' automatically uses 'delta' in calculations)
+        velocity = MoveAndSlide(velocity, Vector2.Up);
+    }
 }
