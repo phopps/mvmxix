d26e288ad100dd94853595c2d7c1cbddae36ec24
phopps <pathopps1@gmail.com>
2023-03-11 10:28:36 -0500

    added logic and variables to sneak2

--- Actor/Player/Sneak2/Sneak2.cs
+++ Actor/Player/Sneak2/Sneak2.cs
@@ -1,48 +1,78 @@
 using Godot;
-// using static Game;
-
-// TODO: Switch to MoveAndSlideWithSnap for movement, wall and ceiling crawling, collision detection raycast, rotate to match surface on collision, attack strength (can be default), special Dash, no need for wall jumping (disable if needed)
 
 public class Sneak2 : KinematicBody2D
 {
-    // [Export] public float
-
+    [Export] public string name;
+    [Export] public int health;
+    [Export] public float gravity;
+    [Export] public float moveSpeed;
+    [Export] public float jumpSpeed;
+    [Export] public Vector2 velocity;
+    [Export] public int jumpsUsed;
+    [Export] public int jumpsRemaining;
+    [Export] public bool isActivePlayer;
+    [Export] public bool justJumped;
+    [Export] public Sprite sprite;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
         GD.Print(Name + " is ready. (Sneak2.cs)");
-
-        // Update inherited player variables
-        // [Export] public float gravity = game.sneakGravity;
-        // moveSpeed = game.sneakMoveSpeed;
-        // jumpSpeed = game.sneakJumpSpeed;
     }
 
-    // public override void AdjustMovementSpeeds()
-    // {
-    //     if (!IsOnFloor())
-    //     {
-    //         // Air movement
-    //         // moveSpeed = game.sneakAirMoveSpeed;
-    //         // jumpSpeed = game.sneakAirJumpSpeed;
-    //     }
-    //     else
-    //     {
-    //         // Floor movement
-    //         // moveSpeed = game.sneakMoveSpeed;
-    //         // jumpSpeed = game.sneakJumpSpeed;
-    //     }
-    // }
+    // Called 60 times per second independent of framerate, delta is time since physics process called.
+    public override void _PhysicsProcess(float delta)
+    {
+        // Reset horizontal velocity
+        velocity.x = 0;
 
-    //FIXME: I need an event with which to play sounds 'on landing'. Where does that go? It is currently not obvious. -Max
+        // Get user input for horizontal movement
+        if (Input.IsActionPressed("right")) { velocity.x += moveSpeed; }
+        if (Input.IsActionPressed("left")) { velocity.x -= moveSpeed; }
 
-    // Move Sneak using move and slide with snap
-    // public override void Move()
-    // {
-    //     // Sneak uses MoveAndSlideWithSnap(velocity, snap, up) instead of MoveAndSlide(velocity, up)
-    //     // Sneak can jump when snap vector is set to Vector.Zero
-    //     // MoveAndSlideWithSnap automatically uses delta in calculations
-    //     // velocity = MoveAndSlideWithSnap(velocity, Vector2.Zero, Vector2.Up);
-    // }
+        // Apply gravity
+        velocity.y += gravity * delta;
+
+        // Get user input for vertical movement
+        if (Input.IsActionPressed("up")) { }
+        if (Input.IsActionPressed("down")) { }
+
+        // Jump on next frame
+        if (Input.IsActionJustPressed("jump"))
+        {
+            // Check if player is on the floor
+            if (IsOnFloor())
+            {
+                // Increment jump counters
+                jumpsUsed++;
+                jumpsRemaining--;
+                justJumped = true;
+
+                // Negative y values are up
+                velocity.y = -jumpSpeed;
+            }
+        }
+
+        // Move the player
+        velocity = MoveAndSlide(velocity, Vector2.Up);
+
+        // Flip sprite to match direction player is moving
+        sprite = GetNode<Sprite>("Sprite");
+
+        if (velocity.x > 0)
+        {
+            // Player is moving right
+            sprite.FlipH = false;
+        }
+        else if (velocity.x < 0)
+        {
+            // Player is moving left
+            sprite.FlipH = true;
+        }
+        else
+        {
+            // Player is not moving
+            // Play idle animation here if it doesn't effect physics or controls
+        }
+    }
 }
